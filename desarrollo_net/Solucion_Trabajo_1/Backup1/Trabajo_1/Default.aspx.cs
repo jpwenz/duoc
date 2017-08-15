e71e505298a1647df4695152a34d59161dddbd66
jpwenz <30914271+jpwenz@users.noreply.github.com>
2017-08-15 16:59:31 -0300

    [#150206738] agregar form sin funcionalidad (#3)

--- desarrollo_net/Solucion_Trabajo_1/Backup1/Trabajo_1/Default.aspx.cs
+++ desarrollo_net/Solucion_Trabajo_1/Backup1/Trabajo_1/Default.aspx.cs
@@ -7,6 +7,9 @@ namespace Trabajo_1
 
     public partial class Default : System.Web.UI.Page
     {
-        
+        public void button1Clicked(object sender, EventArgs args)
+        {
+            button1.Text = "You clicked me";
+        }
     }
 }
